1537090ae37ef1c3c23616a8c987fac61701d101
yoges <94917933+Dinesh-Vishwakarma@users.noreply.github.com>
2025-09-20 22:05:07 +0530

    Bug fixing

--- js/protect.js
+++ js/protect.js
@@ -1,6 +1,5 @@
 (function () {
   const token = localStorage.getItem('token');
-  console.log("PROTECT.JS: Token is", token); // ✅ This will help debug
 
   if (!token || token === 'undefined' || token === 'null' || token.length < 10) {
     window.location.href = 'index.html';
