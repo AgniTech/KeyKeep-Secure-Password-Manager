faa53ddf4d1aa4f07a98dda45ae211e3077cc11b
yoges <94917933+Dinesh-Vishwakarma@users.noreply.github.com>
2025-09-20 20:59:20 +0530

    FIX

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import { app } from '@azure/functions';
 
+// Import all other function files to register them with the runtime
+import './auth/login.js';
+import './auth/register.js';
+import './auth/unlock.js';
+import './user/profile.js';
+import './user/profile-picture.js';
+import './vault/delete.js';
+import './vault/fetch.js';
+import './vault/get.js';
+import './vault/save.js';
+
+// This is a simple health-check endpoint for the API root
 app.http('apiIndex', {
     methods: ['GET'],
     authLevel: 'anonymous',
